663e0a6761b2c02558588105ec13436a7b2be015
Valmiki Vishwakarma <you@example.com>
2022-11-17 00:49:39 +0530

    Initial commit

---
+++ Weather.java
@@ -0,0 +1,12 @@
+import static  io.rest.restassured.RestAssured.*;
+
+public class Weather {
+
+    public static void main(String[] args) {
+
+       System.out.println("Hello World!!");
+
+        RestAssured
+
+    }
+}

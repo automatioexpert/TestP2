6c0bc319a1f2e1d803fd476e8ec3b9998c5f902c
Valmiki Vishwakarma <you@example.com>
2022-11-17 11:30:23 +0530

    Committing  TestCode.java with code changes.Nov17!!

---
+++ src/main/Nov17/TestCode.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestCode {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rollbar.com/login/");
+        System.out.println(driver.findElement(By.cssSelector(".logo-link")).isDisplayed());
+        System.out.println(driver.findElement(By.cssSelector("button[type='submit']")).getText());
+        System.out.println(driver.findElement(By.cssSelector("div.login-options")).getText());
+        driver.quit();
+
+    }
+}

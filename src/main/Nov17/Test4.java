26351035424e07d17f412a190ae439dca7a780ba
Valmiki Vishwakarma <you@example.com>
2022-11-17 11:11:18 +0530

    Committing  Test4.java with code changes.Nov17!!

---
+++ src/main/Nov17/Test4.java
@@ -0,0 +1,10 @@
+public class Test4 {
+
+    public static void main(String[] args){
+        String string = "String237237";
+
+        for(Character character:string.toCharArray()){
+            System.out.println(character);
+        }
+    }
+}
